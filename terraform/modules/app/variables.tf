c32b39e261a1166c6c22c2f25636ead43e36a853
Maksim Soldatov <mnsold.otus@gmail.com>
2019-04-16 22:58:56 +0300

    add modules: app,db,vpc

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,16 @@
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable zone {
+  description = "Zone"
+
+  # Значение по умолчанию
+  default = "europe-west1-b"
+}
+
+variable app_disk_image { 
+  description = "Disk image for reddit app" 
+  default = "reddit-app-base" 
+}
