ae0791e90d1c5a309ca088753d001df118feaddb
Maksim Soldatov <mnsold.otus@gmail.com>
2019-05-04 19:22:06 +0300

    add check mongodb port

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -16,3 +16,7 @@ def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+def test_mongo_listening_socket(host):
+    socket_mongo = host.socket('tcp://0.0.0.0:27017')
+    assert socket_mongo.is_listening
